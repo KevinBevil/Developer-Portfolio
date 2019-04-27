d8e0c2d4538e4a5943f88025bc87549a4caa6998
Kevin Bevil <kbevil@gmail.com>
2019-04-26 20:47:06 -0400

    Progress on portfolio pics and generic programmer pic

--- js/custom.js
+++ js/custom.js
@@ -29,7 +29,7 @@ $(function () {
         dl = $('.color-option .dl li'),
         // Typed Plugin
         typed = new Typed('.type', {
-            strings: ["Frontend Developer.", "Web Designer.", "Graphic Designer."], /* Here Type Your Title */
+            strings: ["Full Stack Dev.", "Front End Dev.", "Node.js Dev.", "MERN Dev."], /* Here Type Your Title */
             typeSpeed: 100,
             loop: true,
             backDelay: 1200,
